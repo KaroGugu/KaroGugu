da24f2fecf25e1ed953f1b430972c453ebd7d7cf
KaroGugu <vgugudis310@gmail.com>
2022-04-02 15:28:57 +0300

    Update folder structure

--- Programming-Fundamentals-Python/k_final_exam/need_for_speed.py
+++ Programming-Fundamentals-Python/k_final_exam/need_for_speed.py
@@ -1,59 +1,52 @@
 number_of_cars = int(input())
-
 cars = {}
 
 for i in range(number_of_cars):
-    car, mileage, fuel = input().split("|")
-    cars[car] = {'mileage': int(mileage), 'fuel': int(fuel)}
+    info = input().split('|')
+    car = info[0]
+    mileage = int(info[1])
+    fuel = int(info[2])
+    cars[car] = [mileage, fuel]
 
 command = input()
+while command != 'Stop':
+    command_data = command.split(" : ")
+    action = command_data[0]
+    car = command_data[1]
 
-while not command == "Stop":
-    split_command = command.split(" : ")
-    command_name = split_command[0]
+    if action == "Drive":
+        distance = int(command_data[2])
+        fuel = int(command_data[3])
 
-    if command_name == "Drive":
-        car, distance, fuel = split_command[1:]
-        distance = int(distance)
-        fuel = int(fuel)
-
-        if cars[car]["fuel"] < fuel:  # You need to drive the given distance, and you will need the given fuel
+        if fuel > cars[car][1]:  # You need to drive the given distance, and you will need the given fuel
             print("Not enough fuel to make that ride")
-
-        else:   # If the car has the required fuel available in the tank,
-            cars[car]["mileage"] += distance  # increase its mileage with the given distance
-            cars[car]["fuel"] -= fuel  # decrease its fuel with the given fuel
+        else:   # If the car has the required fuel available in the tank
+            cars[car][0] += distance  # increase its mileage with the given distance
+            cars[car][1] -= fuel  # decrease its fuel with the given fuel
             print(f"{car} driven for {distance} kilometers. {fuel} liters of fuel consumed.")
 
-        if cars[car]["mileage"] > 100000:
+        if cars[car][0] >= 100000:
             print(f"Time to sell the {car}!")
-            cars.pop(car)
+            del cars[car]
 
 
-    elif command_name == "Refuel":
-        car, fuel = split_command[1:]
-        fuel = int(fuel)
+    elif action == 'Refuel':
+        fuel = int(command_data[2])
+        if cars[car][1] + fuel > 75:   # Each tank can hold a maximum of 75 liters of fuel
+            fuel = 75 - cars[car][1]
+        cars[car][1] += fuel  # Refill the tank of your car
+        print(f"{car} refueled with {fuel} liters")
 
-        cars[car]["fuel"] += fuel  # Refill the tank of your car
-        if cars[car]["fuel"] > 75:   # Each tank can hold a maximum of 75 liters of fuel
-            cars[car]["fuel"] = 75
-            print(f"{car} refueled with {75 - cars[car]['fuel']} liters")
+    elif action == 'Revert':
+        kilometers = int(command_data[2])
+        if cars[car][0] - kilometers < 10000:  # If the mileage becomes less than 10 000km after it is decreased
+            cars[car][0] = 10000   # just set it to 10 000km
         else:
-            print(f"{car} refueled with {fuel} liters")
-
-
-    elif command_name == "Revert":
-        car, kilometers = split_command[1:]
-        kilometers = int(kilometers)
-
-        cars[car]["mileage"] -= kilometers  # Decrease the mileage of the given car with the given kilometers
-        if cars[car]["mileage"] < 10000:  # If the mileage becomes less than 10 000km after it is decreased
-            cars[car]["mileage"] = 10000       # just set it to 10 000km
-            break
-
-        print(f"{car} mileage decreased by {kilometers} kilometers")
+            cars[car][0] -= kilometers  # Decrease the mileage of the given car with the given kilometers
+            print(f"{car} mileage decreased by {kilometers} kilometers")
 
     command = input()
 
-for car in cars:
-    print(f"{car} -> Mileage: {cars[car]['mileage']} kms, Fuel in the tank: {cars[car]['fuel']} lt.")
+
+for key, value in cars.items():
+    print(f"{key} -> Mileage: {value[0]} kms, Fuel in the tank: {value[1]} lt.")
