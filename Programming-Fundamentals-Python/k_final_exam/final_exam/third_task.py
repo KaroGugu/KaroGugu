31d9da76f9afccf256fdaab946ef4153b20b07a9
KaroGugu <vgugudis310@gmail.com>
2022-04-07 12:12:30 +0300

    Task 03. Hero Recruitment from Programming Fundamentals Final Exam - updated

--- Programming-Fundamentals-Python/k_final_exam/final_exam/third_task.py
+++ Programming-Fundamentals-Python/k_final_exam/final_exam/third_task.py
@@ -3,39 +3,43 @@ command = input()
 heroes = {}
 
 while not command == "End":
-    split_command = command.split()
+    split_command = command.split(" ")
 
     if split_command[0] == "Enroll":
         hero_name = split_command[1]
-        if hero_name not in heroes:
-            heroes[hero_name] = hero_name
-        else:
+        if hero_name in heroes:
             print(f"{hero_name} is already enrolled.")
+        else:
+            heroes[hero_name] = []
 
     elif split_command[0] == "Learn":
-        hero_name, spell_name = split_command[1:]
-
+        hero_name = split_command[1]  # hero_name, spell_name = split_command[1:]
+        spell_name = split_command[2]
         if hero_name not in heroes:
             print(f"{hero_name} doesn't exist.")
-        elif spell_name in heroes:
-            print(f"{hero_name} has already learnt {spell_name}.")
-        else:
-            heroes[hero_name] = spell_name
+
+        if hero_name in heroes:
+            if spell_name in heroes[hero_name]:
+                print(f"{hero_name} has already learnt {spell_name}.")
+
+            elif spell_name not in heroes[hero_name]:
+                heroes[hero_name].append(spell_name)
 
     elif split_command[0] == "Unlearn":
-        hero_name, spell_name = split_command[1:]
-
+        hero_name = split_command[1]  # hero_name, spell_name = split_command[1:]
+        spell_name = split_command[2]
         if hero_name not in heroes:
             print(f"{hero_name} doesn't exist.")
-        elif spell_name not in heroes:
-                print(f"{hero_name} doesn't know {spell_name}.")
-        else:
-            del heroes[spell_name]
 
+        if hero_name in heroes:
+            if spell_name not in heroes[hero_name]:
+                print(f"{hero_name} doesn't know {spell_name}.")
+            elif spell_name in heroes[hero_name]:
+                heroes[hero_name].remove(spell_name)
 
     command = input()
 
 print("Heroes:")
 
-for hero, spell in heroes.items():
-    print(f"== {hero}: {spell}")
+for (key, value) in heroes.items():
+    print(f"== {key}: {(', '.join(value))}")
