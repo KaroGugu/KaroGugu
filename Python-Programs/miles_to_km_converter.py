a5a0db0a2ed6795cff279446575a4db0dceb95ac
KaroGugu <vgugudis310@gmail.com>
2022-04-23 14:48:29 +0300

    Python Program for calculating miles into km

--- Python-Programs/miles_to_km_converter.py
+++ Python-Programs/miles_to_km_converter.py
@@ -2,7 +2,7 @@ from tkinter import *
 
 def miles_to_km():
     miles = float(miles_input.get())
-    km = miles * 1.609
+    km = miles * 1.609344
     km_result_label.config(text=f"{km}")
 
 window = Tk()
